791213bd284a6644dce0b3871cda43441c0cd549
Muntaser Abukhadijah <mabukhad@amazon.com>
2025-08-24 18:55:05 +0300

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :blog_posts
+
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update", as: :update_blog_post
+  # delete "/blog_posts/:id", to: "blog_posts#destroy", as: :destroy_blog_post
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  # Defines the root path route ("/")
+  root "blog_posts#index"
+end
